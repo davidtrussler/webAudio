f66249caf29dd03654cbf9e80cc05de41d12695c
David Trussler <mail@davidtrussler.net>
2016-10-26 15:00:52 +0100

    Comment: Loads sound sample

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a basic implementation of the WebAudio API set up for general experimentation.
+
+based on the tutorial here: https://www.html5rocks.com/en/tutorials/webaudio/intro/
--- webAudio.js
+++ webAudio.js
@@ -1,4 +1,6 @@
 var context;
+var soundURL = 'sounds/COSM114.mp3';
+var soundBuffer = null;
 
 window.addEventListener(
   'load', init, false
@@ -10,7 +12,24 @@ function init() {
   try {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     context = new window.AudioContext();
+    loadSound(soundURL);
   } catch(e) {
     console.log('WebAudio API is not supported in this browser!');
   }
+}
+
+function loadSound(soundURL) {
+  console.log('loadSound!');
+
+  var request = new XMLHttpRequest();
+
+  request.open('GET', soundURL, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      soundBuffer = buffer;
+    });
+  }
+
+  request.send();
 }
