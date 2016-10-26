44e3eb1dd7a7c59edaa9a212e9d6705b034e16e3
David Trussler <mail@davidtrussler.net>
2016-10-26 16:32:31 +0100

    Comment: Set up play sound function

--- js/webAudio.js
+++ js/webAudio.js
@@ -6,53 +6,40 @@ window.addEventListener(
 );
 
 function init() {
-  console.log('init!');
   setUpContext();
   setUpLinks();
-}
-
-function setUpLinks() {
-  console.log('setUpLinks!');
-
-  var i = 0;
-
-  $('.soundLink').each(function() {
-    console.log('link: ', this);
-
-    loadSound(this.href, i);
-
-    $(this).click(function(e) {
-      e.preventDefault();
-      playSound(i);
-    });
-
-    i++;
-  });
 }
 
 function setUpContext() {
   try {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     context = new window.AudioContext();
-
-    console.log('context created!');
-
-    // loadSound(soundURL);
   } catch(e) {
     console.log('WebAudio API is not supported in this browser!');
   }
 }
 
-function loadSound(soundURL, i) {
-  console.log('loadSound!');
+function setUpLinks() {
+  $('.soundLink').each(function() {
+    var name = this.href.split('/')[this.href.split('/').length - 1].split('.')[0];
 
+    loadSound(this.href, name);
+
+    $(this).click(function(e) {
+      e.preventDefault();
+      playSound(name);
+    });
+  });
+}
+
+function loadSound(soundURL, name) {
   var request = new XMLHttpRequest();
 
   request.open('GET', soundURL, true);
   request.responseType = 'arraybuffer';
   request.onload = function() {
     context.decodeAudioData(request.response, function(buffer) {
-      soundBuffers[i] = buffer;
+      soundBuffers[name] = buffer;
       console.log('sound loaded!')
     });
   }
@@ -61,8 +48,14 @@ function loadSound(soundURL, i) {
 }
 
 
-function playSound() {
-  console.log('playSound!');
+function playSound(name) {
+  console.log(name);
+
+  var source = context.createBufferSource();
+
+  source.buffer = soundBuffers[name];
+  source.connect(context.destination);
+  source.start(0);
 }
 
 console.log('soundBuffers: ', soundBuffers);
